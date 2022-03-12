e6b9b20094a5152f1c5159f33fd86197266ef3ae
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-03-12 16:56:54 +0300

    Done

--- src/features/quotes/QuoteForm.js
+++ src/features/quotes/QuoteForm.js
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 import { addQuote } from "./quotesSlice";
+import { useDispatch } from "react-redux";
 
 function QuoteForm() {
+  const dispatch = useDispatch()
   const [formData, setFormData] = useState({
+    author: "",
+    content: ""
     // set up a controlled form with internal state
     // look at the form to determine what keys need to go here
   });
 
   function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    })
     // Handle Updating Component State
   }
 
   function handleSubmit(event) {
+    event.preventDefault()
+    dispatch(
+      addQuote({
+        ...formData,
+        id: uuid(),
+        votes: 0
+      })
+    )
+    setFormData({
+      author: "",
+      content: ""
+    })
     // Handle Form Submit event default
     // Create quote object from state
     // Pass quote object to action creator
@@ -25,16 +45,18 @@ function QuoteForm() {
         <div className="col-md-8 col-md-offset-2">
           <div className="panel panel-default">
             <div className="panel-body">
-              <form className="form-horizontal">
+              <form className="form-horizontal" onSubmit={handleSubmit}>
                 <div className="form-group">
                   <label htmlFor="content" className="col-md-4 control-label">
                     Quote
                   </label>
                   <div className="col-md-5">
-                    <textarea
+                  <textarea
                       className="form-control"
+                      name="content"
                       id="content"
                       value={formData.content}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -43,11 +65,13 @@ function QuoteForm() {
                     Author
                   </label>
                   <div className="col-md-5">
-                    <input
+                  <input
                       className="form-control"
                       type="text"
+                      name="author"
                       id="author"
                       value={formData.author}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
